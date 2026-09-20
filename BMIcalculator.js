2e2a1c45ddc9ce0158100e9b0633832029c03cf4
risika <risikasahu76@gmail.com>
2026-09-20 13:51:19 +0530

    project2 BMI

---
+++ BMIcalculator.js
@@ -0,0 +1,40 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const height= parseInt(document.querySelector('#height').value);
+
+    const weight= parseInt(document.querySelector('#weight').value);
+
+    const results= document.querySelector('#results');
+
+    const guide=document.querySelector('#guide');
+
+    if(height === '' || height<0 || isNaN(height)){
+        results.innerHTML = `please give a valid height ${height}`;
+
+    }
+    else if(weight === '' || weight<0 || isNaN(weight)){
+        results.innerHTML = `please give a valid weight ${weight}`;
+    }
+    else{
+       const bmi = (weight / ((height *height ) / 10000)).toFixed(2);
+
+       results.innerHTML=`<span> ${bmi}</span>`
+
+       if(bmi<18.6){
+        guide.innerHTML="under weight";
+
+       }
+       else if(bmi>=18.6 && bmi<24.9){
+        guide.innerHTML="Normal range"
+       }
+       else{
+        guide.innerHTML="overweight";
+       }
+       
+       
+    }
+
+});
